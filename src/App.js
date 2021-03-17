4183b73a9890e8ceffe98151ba2bf9598890c71f
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-03-16 22:46:29 -0700

    Added components and updated App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+import Search from "./Search";
+import City from "./City";
+import Description from "./Description";
+import CurrentIcon from "./CurrentIcon";
+import TemperatureNow from "./TemperatureNow";
+import FeelsLike from "./FeelsLike";
+import Footer from "./Footer";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <Search />
+            <City />
+            <Description />
+            <CurrentIcon />
+            <TemperatureNow />
+            <FeelsLike />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
-
-export default App;
