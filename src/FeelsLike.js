3d7b1a38afd64a9c039a053227e5045b7d053d55
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-03-16 22:54:51 -0700

    Updated FeelsLike and Footer components

---
+++ src/FeelsLike.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FeelsLike.css";
+
+export default function FeelsLike() {
+  return (
+    <div>
+      <h3 className="temperature-like" id="feels-like">
+        Feels like <strong>34°</strong>
+      </h3>
+    </div>
+  );
+}
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a href="https://github.com/hueyteng/my-weather-project" target="_blank">
+        Open source code
+      </a>{" "}
+      by Huey Teng Tay
+    </div>
+  );
+}
