567dac34e60a2d5bc1d59c4b47473e9e8b61822b
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-03-16 22:51:27 -0700

    Updated CurrentIcon component

---
+++ src/CurrentIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CurrentIcon.css";
+
+export default function CurrentIcon() {
+  let weatherData = {
+    imgUrl: "/Images/01d.png"
+  };
+  return (
+    <div className="CurrentIcon">
+      <img
+        class="current-icon"
+        src={weatherData.imgUrl}
+        alt="weather-image"
+        id="weather-icon"
+      />
+    </div>
+  );
+}
