2dfade8c92d90c78722a6f7852c40013e9f5929a
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-03-18 08:46:01 -0700

    Edited open source link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/hueyteng/my-weather-project" target="_blank" rel="noreferrer">
+      <a href="https://github.com/hueyteng/weatherapp-react" target="_blank" rel="noreferrer">
         Open source code
       </a>{" "}
       by Huey Teng Tay
