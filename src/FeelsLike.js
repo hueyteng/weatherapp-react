cfc2ccfe987e4ac9f319a605da13b887ba0a8d50
hueyteng <78068539+hueyteng@users.noreply.github.com>
2021-03-24 23:51:47 -0700

    Attempted to integrate unit conversion to FeelsLike component

--- src/FeelsLike.js
+++ src/FeelsLike.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./FeelsLike.css";
+import "./WeatherTemperature";
 
 export default function FeelsLike(props) {
+
+  function celsius() {
+    let temp = Math.round(props.data.feelsLike);
+    return `${temp}°`;
+  }
+
+  function fahrenheit() {
+    let temp = Math.round(props.data.feelsLike * 9 / 5) + 32;
+    return `${temp}°`;
+  }
+
+  if (props.unit === "celsius") {
   return (
-    <div className="FeelsLike">
-      <h3>
-        Feels like <strong> {props.data.feelsLike}° </strong>
-      </h3>
-    </div>
+      <div className="FeelsLike">
+        <h3>
+          Feels like <strong> {celsius()} </strong>
+        </h3>
+      </div>
   );
+
+  } else {
+    return (
+      <div className="FeelsLike">
+        <h3>
+          Feels like <strong> {fahrenheit()} </strong>
+        </h3>
+      </div>
+  );
+  }
 }
